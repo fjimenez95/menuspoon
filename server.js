d22987ca8367b586bed8a517148cbd0a2d33c1ec
Freddy Jimenez <freddy.junior@gmail.com>
2020-11-30 20:29:22 -0500

    required session, passport, dotenv

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+
+
+require('dotenv').config();
 
 // ASSIGN ROUTERS
 const indexRouter = require('./routes/index');
@@ -15,12 +20,24 @@ const app = express();
 
 // CONFIGURE SERVER SETTINGS
 require('./config/database'); // REQUIRING THE DATABASE
+require('./config/passport'); // REQUIRING PASSPORT
 app.set('view engine', 'ejs');
 
 // MOUNT MIDDLEWARE app.use()
 app.use(morgan('dev')); // HTTP LOGGER MIDDLEWARE
 app.use(express.static('public')); // STATIC MIDDLEWARE
-app.use(express.urlencoded({ extended: false })); // BODY PARSER MIDDLEWARE
+app.use(express.urlencoded({ extended: false })); // BODY PARSER MIDDLEWARE - QUESTION: DOES THIS NEED TO BE TRUE??
+
+// MIDDWARE FOR GOOGLE OAUTH
+app.use(session({
+  secret: 'menuspoon-rocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// MOUNT PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
 
 // TODO: MOUNT ROUTES
 app.use('/', indexRouter);
